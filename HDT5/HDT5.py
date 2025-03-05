57736906802ed03f5c20bf98b85e8c30a5db6b99
jsam1904 <javierse.alvarado@gmail.com>
2025-03-05 10:40:16 -0600

    Implementacion de comentarios en el codigo

--- HDT5/HDT5.py
+++ HDT5/HDT5.py
@@ -11,14 +11,28 @@ RAM_CAPACIDAD = 100
 CPU_VELOCIDAD = 3  # Instrucciones por unidad de tiempo
 TIME = 1000  # Tiempo total de simulación
 
+# Autores: Javier Alvarado(24546), Jonathan Tubac(24484), Juan Montenegro(24750)
+
 class SistemaOperativo:
     def __init__(self, env, ram_capacidad, cpu_velocidad):
+        """
+        Inicializa el sistema operativo con los recursos de RAM y CPU.
+        
+        :param env: El entorno de simulación de SimPy.
+        :param ram_capacidad: La capacidad total de la RAM.
+        :param cpu_velocidad: La velocidad de la CPU en instrucciones por unidad de tiempo.
+        """
         self.env = env
         self.ram = simpy.Container(env, init=ram_capacidad, capacity=ram_capacidad)
         self.cpu = simpy.Resource(env, capacity=1)
         self.cpu_velocidad = cpu_velocidad
 
     def ejecutar_proceso(self, proceso):
+        """
+        Ejecuta un proceso en el sistema operativo.
+        
+        :param proceso: El proceso a ejecutar.
+        """
         yield self.ram.get(proceso.memoria)
         with self.cpu.request() as req:
             yield req
@@ -32,6 +46,12 @@ class SistemaOperativo:
 
 class Proceso:
     def __init__(self, env, nombre):
+        """
+        Inicializa un nuevo proceso.
+        
+        :param env: El entorno de simulación de SimPy.
+        :param nombre: El nombre del proceso.
+        """
         self.env = env
         self.nombre = nombre
         self.instrucciones = random.randint(1, 10)
@@ -39,12 +59,27 @@ class Proceso:
         self.tiempo_llegada = env.now
 
 def generar_procesos(env, so, num_procesos, intervalo):
+    """
+    Genera procesos en el entorno de simulación a intervalos regulares.
+    
+    :param env: El entorno de simulación de SimPy.
+    :param so: El sistema operativo que ejecutará los procesos.
+    :param num_procesos: El número de procesos a generar.
+    :param intervalo: El intervalo de tiempo entre la generación de procesos.
+    """
     for i in range(num_procesos):
         proceso = Proceso(env, 'Proceso'+ str(i))
         env.process(so.ejecutar_proceso(proceso))
         yield env.timeout(random.expovariate(1.0 / intervalo))
 
 def ejecutar_simulacion(num_procesos, intervalo):
+    """
+    Ejecuta la simulación de procesos y calcula el tiempo promedio y la desviación estándar.
+    
+    :param num_procesos: El número de procesos a simular.
+    :param intervalo: El intervalo de tiempo entre la generación de procesos.
+    :return: El tiempo promedio y la desviación estándar de los tiempos en el sistema.
+    """
     global tiempos
     tiempos = []
     env = simpy.Environment()
